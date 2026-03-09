a599945a40ef8f8dc9f569bd101f080d63eeab92
EricMontez <eric.alexander.montez@gmail.com>
2026-03-09 14:10:21 -0700

    A34 Done

--- main.cpp
+++ main.cpp
@@ -8,9 +8,12 @@ int main()
   double avg;
 
   // TODO: Read three months and their rainfall values from standard input
-
+  cout << "Enter three months and their rainfall values: (eg. Jan 2.5, Feb 3.0, Mar 1.5)" << endl;
+  cin >> month1 >> rainfall1 >> month2 >> rainfall2 >> month3 >> rainfall3;
   // TODO: Calculate the average rainfall
+  avg = (rainfall1 + rainfall2 + rainfall3) / 3;
 
   cout << fixed << setprecision(2);
   // TODO: Print the result in the required format
+  cout << "The average rainfall for " << month1 << ", " << month2 << ", and " << month3 << " is: " << avg << " inches" << endl;
 }
